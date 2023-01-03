f10db2b4092962a7e8e59c6f3eaef0b364a026be
canyagmur <can.yagmur@ozu.edu.tr>
2023-01-03 23:39:48 +0300

    add new tests

--- can-project/can-project/src/test/java/com/example/canproject/CanProjectApplicationTests.java
+++ can-project/can-project/src/test/java/com/example/canproject/CanProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.canproject;
 
+import com.example.canproject.DTO.AvailableCarByTypeDTO;
 import com.example.canproject.DTO.CarCreaterDTO;
 import com.example.canproject.entity.Car;
 import com.example.canproject.service.CarService;
@@ -18,15 +19,72 @@ class CanProjectApplicationTests {
 	void testFindByCarBarcode(){
 		Car car;
 		car = new Car();
+		car.setBarcode(15L);
 		car.setBrand("Ferrari");
 		car.setModel("488 Pista");
-		car.setBarcode(3L);
 
 		CarService carService = new CarService();
-		CarCreaterDTO carResult = carService.findByBarcode(3L);
+		CarCreaterDTO carResult = carService.findByBarcode(15L);
 
 		assertNotNull(carResult);
 		assertEquals(car.getBarcode(), String.valueOf(carResult.getBarcode()));
+	}
+
+	/*
+        public CarCreaterDTO(){
+
+    }
+     */
+	@Test
+	void saveCar(){
+		Car car = new Car();
+		car.setBarcode(13L);
+		car.setBrand("Ferrari");
+		car.setModel("F40");
+
+
+		CarService carService = new CarService();
+		carService.save(new CarCreaterDTO());
+
+
+	}
+
+	@Test
+	void deleteCars(){
+		Car car = new Car();
+		car.setBarcode(12L);
+		car.setBrand("Ferrari");
+		car.setModel("F12");
+
+		CarService carService =  new CarService();
+		carService.deleteById(car.getBarcode());
+	}
+
+	@Test
+	void getAllAvailableCars(){
+		Car car = new Car();
+		car.setBarcode(13L);
+		car.setBrand("Ferrari");
+		car.setModel("812 Compitozone");
+
+		CarService carService =  new CarService();
+		carService.getAvailableCars(new AvailableCarByTypeDTO(car.getTransmissionType(), car.getType().toString()));
+
+	}
+	@Test
+	void getRentedCars(){
+		Car car = new Car();
+		car.setBarcode(13L);
+		car.setBrand("Porsche");
+		car.setModel("911 GT3");
+
+
+		CarService carService =  new CarService();
+		CarCreaterDTO carResult = (CarCreaterDTO) carService.getAllRentedCars();
+
+		assertNotNull(carResult);
+
+
 	}
 
 }
